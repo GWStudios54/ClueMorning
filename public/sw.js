e35d3a055b0fa620e633344402ac2dc50046954e
GWStudios54 <aarin54@gmail.com>
2026-09-12 15:36:11 -0700

    Refresh the PWA shell for final icon alignment

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'clue-morning-pwa-v50';
+const CACHE = 'clue-morning-pwa-v51';
 const APP_SHELL = [
   '/styles.css',
   '/styles-base.css',
@@ -57,7 +57,7 @@ self.addEventListener('activate', event => {
       try {
         const url = new URL(client.url);
         if (url.origin === self.location.origin && (url.pathname === '/' || url.pathname === '/index.html')) {
-          return client.navigate('/?shell=v50');
+          return client.navigate('/?shell=v51');
         }
       } catch {}
       return undefined;
