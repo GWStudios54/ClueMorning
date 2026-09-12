9cf006ec85a56123a2ded184a250f5dea3219033
GWStudios54 <aarin54@gmail.com>
2026-09-12 12:52:31 -0700

    Refresh the PWA shell for homepage control fixes

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'clue-morning-pwa-v47';
+const CACHE = 'clue-morning-pwa-v48';
 const APP_SHELL = [
   '/styles.css',
   '/styles-base.css',
@@ -57,7 +57,7 @@ self.addEventListener('activate', event => {
       try {
         const url = new URL(client.url);
         if (url.origin === self.location.origin && (url.pathname === '/' || url.pathname === '/index.html')) {
-          return client.navigate('/?shell=v47');
+          return client.navigate('/?shell=v48');
         }
       } catch {}
       return undefined;
