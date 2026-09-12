3fd8b09c37f651167680e28c2e5cce6a135ab539
GWStudios54 <aarin54@gmail.com>
2026-09-12 12:46:57 -0700

    Refresh the PWA shell for contrast fixes

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'clue-morning-pwa-v46';
+const CACHE = 'clue-morning-pwa-v47';
 const APP_SHELL = [
   '/styles.css',
   '/styles-base.css',
@@ -57,7 +57,7 @@ self.addEventListener('activate', event => {
       try {
         const url = new URL(client.url);
         if (url.origin === self.location.origin && (url.pathname === '/' || url.pathname === '/index.html')) {
-          return client.navigate('/?shell=v46');
+          return client.navigate('/?shell=v47');
         }
       } catch {}
       return undefined;
